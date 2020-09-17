3d34ea3f4f9ecd38d181836d57b8f4814c747567
SiriVH <siri.haugum@uib.no>
2020-09-17 12:19:41 +0200

    Added anosim and adonis

--- Ordination_full.R
+++ Ordination_full.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(vegan)
-library(ggplot2)
+#library(ggplot2)
 library(readxl)
+#library(data)
 
 # Need rutedata sheet 'frequency' for species abundances. Call it 'veg'
 # Need rutedata sheet 'names' for taxa. Call it 'taxa'
@@ -9,33 +10,39 @@ library(readxl)
 
 veg <- readRDS('cleandata/community.rds') #community data cleaned
 PlotID <- read_xlsx('Data/drought.plots.xlsx')
-taxa <- read_xlsx('Data/DE.1_Community.xlsx', sheet = 'names')
+taxa <- read_xlsx('Data/DE.1_Community_2020.xlsx', sheet = 'names')
 
 prep <- veg %>%
-  filter (site == 'LYG') %>%                             
+ # filter (site == 'LYG') %>%  
+  filter(experiment == 'drought' | experiment == 'Drought') %>% 
   replace (. == '.+' | . == 'plas', 1) %>%              # Replace <1 abundances with 1
-  rename (Latin_name = species) %>%                    
+  dplyr::rename(Latin_name = species) %>%                    
   left_join (taxa, by='Latin_name') %>% 
   filter (!(Group=='bryophyte' | Group=='lichen')) %>% 
-  rename (species = Latin_name) %>% 
-  select (year, plot, species, cover) %>% 
+  dplyr::rename (species = Latin_name) %>% 
+  #select (year, plot, species, cover) %>% 
+  group_by(site, year, plot, species) %>% 
+  summarise_at(.vars = 'cover', sum, na.rm = TRUE) %>% 
   spread (species, cover, fill = NA, convert = FALSE, drop = TRUE, sep = NULL) %>% 
-  mutate_each (funs(as.numeric), 3:54) %>% 
-  replace (is.na(.), 0) 
+  #mutate_each (funs(as.numeric), 4:53) %>% 
+  replace (is.na(.), 0) %>% 
+  ungroup(year, site, plot)
 
 com <- metaMDS(prep %>%
-                 select (-year, -plot))
+                 dplyr::select (-year, -plot, -site))
 
 env <- prep %>% 
-  select (year, plot) %>% 
+  dplyr::select (year, plot) %>% 
   mutate (year = as.character(year)) %>% 
   left_join (PlotID, by='plot') %>% 
-  mutate(phase = fct_recode(phase, "Young" = 'pioneer', "Intermediate" = 'building', 'Old' = 'mature'))
+  mutate(phase = fct_recode(phase, "Young" = 'pioneer', "Intermediate" = 'building', 'Old' = 'mature'),
+         geography = fct_recode(geography, "North" = 'north', "South" = 'south'))
 
 data.scores <- as.data.frame(scores(com)) %>%   #Using the scores function from vegan to extract the site scores and convert to a data.frame
                  mutate(plot = prep$plot,
                         year = as.character(prep$year))%>% 
-                 left_join(env, by=c('plot', 'year'))
+                 left_join(env, by=c('plot', 'year')) 
+
 
 data.scores$phase<-factor(data.scores$phase, levels=c("Young", "Intermediate", "Old"))
 levels(data.scores$phase) <- c("Young", "Intermediate", "Old") 
@@ -44,17 +51,29 @@ species.scores <- as.data.frame(scores(com, "species")) %>% #Using the scores fu
                   mutate(species = rownames(.))
   
 reddata <- data.scores %>% 
-  select(-phase) 
+  mutate(region = geography) %>% 
+  dplyr::select(-phase, -geography) 
+
+reddata1 <- data.scores %>% 
+ filter(geography == 'North') %>% 
+  dplyr::select(-phase, -geography) 
+
+reddata2 <- data.scores %>% 
+  filter(geography == 'South') %>% 
+  dplyr::select(-phase, -geography)
 
 
-p <-
+#p <-
 ggplot(data=data.scores,aes(x=NMDS1,y=NMDS2, shape= year, colour= treatment ),alpha=0.5 ) + 
-  geom_point(data= reddata, colour = 'grey', size=2) +
-  geom_point(size=3) +
+  geom_point(data= reddata1, colour = 'gray75', size=5) +
+  geom_point(data= reddata2, colour = 'gray90', size=5) +
+  geom_point(size = 6) +
   #geom_path(data=reddata,aes(x=NMDS1,y=NMDS2, group=plot)) +
   #scale_color_brewer(palette="Set1") +
-  scale_colour_manual(values=c("0" = "steelblue2", "50" = "springgreen4", "90" = "orange2"), name = "Drought frequency", labels = c("Ambient", "Moderate", "Extreme")) +
-  scale_shape_manual(values=c(16, 17, 15, 18))+
+  #scale_colour_manual(values=c("0" = "#0072B2", "60" = "#F0E442", "90" = "#D55E00"), name = "Drought frequency", labels = c("Ambient", "Moderate", "Extreme")) +
+  scale_colour_manual(values = c("0" = 'darkblue', "60" = 'deepskyblue3', "90" = "mediumturquoise"), 
+                      name = "Drought intensity", labels = c("Ambient", "Moderate", "Extreme")) +
+  scale_shape_manual(values=c(19, 19, 19, 19, 18))+
   coord_equal() +
   geom_path(data=data.scores,aes(x=NMDS1,y=NMDS2, group=plot)) +
   theme(panel.grid.major = element_blank(), 
@@ -63,12 +82,62 @@ ggplot(data=data.scores,aes(x=NMDS1,y=NMDS2, shape= year, colour= treatment ),al
         axis.line = element_line(colour = "black"),
         legend.background = element_blank(),
         legend.key = element_blank(),
-        strip.text.x = element_text(size = 14),
-        #strip.background =element_blank()
+        legend.position="bottom",
+        legend.box = "vertical",
+        legend.text = element_text(size = 30),
+        legend.title = element_text(size = 30),
+        #legend.box.just = 'bottom',
+        legend.margin=unit(0.5, "cm"),
+        strip.text = element_text(size = 30),
+        strip.background =element_blank(),
+        axis.title = element_text(size = 30),
+        axis.text = element_text(size = 20)
         )+
   labs(shape="Year", colour="Treatment") +
-  facet_wrap(~phase) 
+  facet_grid(geography~phase) 
 
-tiff("Figures/ordination.tiff", units="in", width=12, height=5, res=300)
+tiff("Figures/ordination_shades.tiff", units="px", width = 6000, height = 4000, res=300)
 p
 dev.off() 
+
+
+# ANlysis of variance
+
+# Species-site matrix
+
+com2 <- prep %>% 
+  dplyr::select(- site, -year, -plot)
+
+
+# Environmental meata data
+
+env2 <- env %>% 
+  mutate(site2 = paste(geography, phase, sep='_')) %>% 
+  mutate(site3 = fct_relevel(site2, levels = c('North_Young', 'North_Intermediate', 'North_Old', 'South_Young', 'South_Intermediate', 'South_Old')))
+
+
+#anosim 
+
+ano <- anosim(com2, env2$site3, permutations = 999, distance = "bray", strata = NULL,
+       parallel = getOption("mc.cores"))
+
+
+#Plot
+
+plot(x=ano$class.vec, y=ano$dis.rank, xlab = 'Sites', ylab = 'Dissimilarity rank', ylim = c(0,40000))
+
+
+tiff('Figures/anosim.tif', units = 'px', height = 1500, width = 3500, res = 300)
+plot(x=ano$class.vec, y=ano$dis.rank, xlab = 'Sites', ylab = 'Dissimilarity rank', ylim = c(0,40000))
+dev.off()
+
+
+# same with adonis
+
+a2 <- adonis2(com2~env2$site2)
+
+
+# check pair-wise dissimilarities
+
+permutest(betadisper(dist(com2), env2$site2), pairwise = TRUE)
+
